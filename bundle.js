ad3706c9d28f1102c662586a1620957cda2edbe0
ys558 <yuyi.gz@163.com>
2022-01-25 11:14:46 +0800

    加入babel,使得打包出来的bundle.js文件经babel转译为浏览器解析对象

--- bundle.js
+++ bundle.js
@@ -2,22 +2,28 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
+var _createClass = require('@babel/runtime/helpers/createClass');
+var _classCallCheck = require('@babel/runtime/helpers/classCallCheck');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var _createClass__default = /*#__PURE__*/_interopDefaultLegacy(_createClass);
+var _classCallCheck__default = /*#__PURE__*/_interopDefaultLegacy(_classCallCheck);
+
 var version = "1.0.0";
 
-const foo = () => {
-    console.log('xxxxxx');   
+var foo = function foo() {
+  console.log('xxxxxx');
 };
 
 function main () {
   console.log('version ' + version);
 }
+var A = /*#__PURE__*/_createClass__default["default"](function A(param) {
+  _classCallCheck__default["default"](this, A);
 
-class A {
-    constructor(param) {
-        this.param = param;
-    }
-}
-
+  this.param = param;
+});
 foo();
 
 exports.A = A;
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import json from "rollup-plugin-json";
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
     input: 'src/main.js',
@@ -6,5 +8,35 @@ export default {
       file: 'bundle.js',
       format: 'cjs'
     },
-    plugins: [ json() ]
+    plugins: [ 
+      json(),
+      // Rollup仅支持ES6模块，此插件是将外部依赖CommonJS模块转换为ES6模块的插件
+      commonjs({
+        include: 'node_modules/**',
+      }),
+      babel({
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: {
+                browsers: 'last 2 versions, > 1%, ie >= 6, Android >= 4, iOS >= 6, and_uc > 9',
+                node: '0.10'
+              },
+              // 是否将ES6模块转为CommonJS模块，必须为false
+              // 否则 Babel 会在 Rollup 有机会做处理之前，将我们的模块转成 CommonJS，导致 Rollup 的一些处理失败
+              // 例如rollup-plugin-commonjs插件，将 CommonJS 转换成 ES6 模块
+              modules: false,
+              // 松散模式，源码不同时使用export和export default时可开启，更好兼容ie8以下
+              loose: false,
+              // 按需进行polyfill
+              useBuiltIns: 'usage'
+            }
+          ]
+        ],
+        plugins: ['@babel/plugin-transform-runtime'],
+        runtimeHelpers: true,
+        exclude: 'node_modules/**'
+      })
+    ]
   };
